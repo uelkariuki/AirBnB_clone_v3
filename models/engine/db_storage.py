76a6bad14daf74ad645c94afa6909d7250301775
abraham-ship <bettabraham11@gmail.com>
2023-09-29 15:11:47 -0700

    improved storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,14 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """a method to retrieve object"""
+        for val in self.all(cls).values():
+            if val.id == id:
+                return val
+        return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        return len(self.all(cls))
