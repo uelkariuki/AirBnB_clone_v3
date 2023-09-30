860f895b560135b86ba4f46dc672ce4c8610855c
abraham-ship <bettabraham11@gmail.com>
2023-09-30 02:08:39 -0700

    added 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,7 +18,7 @@ def close(self):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
