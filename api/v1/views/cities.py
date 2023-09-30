14bc28de7496c77dc8deea894ed57b818485d514
abraham-ship <bettabraham11@gmail.com>
2023-09-30 07:51:04 -0700

    city objects

---
+++ api/v1/views/cities.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+'''handle all default RESTFul API actions for City objects'''
+from flask import abort, jsonify
+from models.city import City
+from models.state import State
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'])
+def get_cities(state_id):
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    cities = [city.to_dict() for city in state.cities]
+    return jsonify(cities)
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'])
+def get_city(city_id):
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route('cities/<city_id>', methods=['DELETE'])
+def delete_city(city_id):
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'])
+def create_city(state_id):
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+    new_city = City(**data)
+    new_city.state_id = state.id
+    new_city.save()
+    return jsonify(new_city.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'])
+def update_city(city_id):
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    for key, value in data.items():
+        if key not in ["id", "state_id", "created_at", "updated_at"]:
+            setattr(city, key, value)
+    city.save()
+    return jsonify(city.to_dict()), 200
