27d7b940fba13711c63dcbbe59ed6c16d5067b88
abraham-ship <bettabraham11@gmail.com>
2023-10-01 03:16:49 -0700

    Reviw

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -18,17 +18,17 @@ def get_reviews_by_place(place_id):
     return jsonify(reviews)
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['GET'],
-                 strict_slashes=False)
-def get_reviews_by_place(place_id):
-    place = storage.get(Place, place_id)
-    if not place:
+@review_view.route('/reviews/<review_id>', methods=['GET'],
+                   strict_slashes=False)
+def get_review(review_id):
+    review = storage.get(Review, review_id)
+    if not review:
         abort(404, "Not found")
-    reviews = [review.to_dict() for review in place.reviews]
-    return jsonify(reviews)
+    return jsonify(review.to_dict())
 
 
-@app_views.route('/<review_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_review(review_id):
     review = storage.get(Review, review_id)
     if not review:
@@ -60,7 +60,7 @@ def create_review(place_id):
     return jsonify(new_review.to_dict()), 201
 
 
-@app_views.route('/<review_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def update_review(review_id):
     review = storage.get(Review, review_id)
     if not review:
