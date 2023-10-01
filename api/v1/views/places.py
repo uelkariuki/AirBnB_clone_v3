cbe2d713dbd14429c453233b3eef1d3fa700b402
uelkariuki <cratzuel@gmail.com>
2023-10-01 13:21:34 +0000

    Create a new view for Place objects that handles all default RESTFul API actions

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,18 +8,19 @@ from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.place import Place
+from models.city import City
+from models.user import User
 """ Importing the required modules"""
 
 
 @app_views.route('/cities/<city_id>/places/', methods=['GET'])
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
-def places_get():
+def Places_get(city_id):
     """ Function that retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    places = [place.to_dict() for place in
-                 storage.all(Place).values()]
+    places = [place.to_dict() for place in city.places]
 
     return jsonify(places)
 
@@ -46,7 +47,7 @@ def delete_place(place_id):
 
 @app_views.route('/cities/<city_id>/places/', methods=['POST'])
 @app_views.route('/cities/<city_id>/places', methods=['POST'])
-def post_place():
+def post_place(city_id):
     """ Creates a Place"""
     city = storage.get(City, city_id)
     if city is None:
@@ -54,15 +55,16 @@ def post_place():
     request_data = request.get_json()
     if not request_data:
         abort(400, description="Not a JSON")
+    if 'user_id' not in request_data:
+        abort(400, description="Missing user_id")
+    user_id = request_data['user_id']
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
-    if 'user_id' not in request_data:
-        abort(400, description="Missing user_id")
     if 'name' not in request_data:
         abort(400, description="Missing name")
-    place = Place(name=request_data['name'])
-    amenity.save()
+    place = Place(name=request_data['name'], city_id=city_id, user_id=user_id)
+    place.save()
     return jsonify(place.to_dict()), 201
 
 
