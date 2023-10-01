52978902026016bc6ed53fad2ab225d926db8555
abraham-ship <bettabraham11@gmail.com>
2023-10-01 01:15:14 -0700

    City

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -17,7 +17,7 @@ def get_cities(state_id):
     return jsonify(cities)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'])
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     city = storage.get(City, city_id)
     if not city:
@@ -25,7 +25,7 @@ def get_city(city_id):
     return jsonify(city.to_dict())
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'])
+@app_views.route('/cities/<city_id>', methods=['DELETE', strict_slashes=False])
 def delete_city(city_id):
     city = storage.get(City, city_id)
     if not city:
@@ -35,7 +35,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'])
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     state = storage.get(State, state_id)
     if not state:
@@ -51,7 +52,7 @@ def create_city(state_id):
     return jsonify(new_city.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'])
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     city = storage.get(City, city_id)
     if not city:
